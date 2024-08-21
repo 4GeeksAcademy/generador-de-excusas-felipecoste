ea7ed4c4a64a41d270d8edef49c56ae5f9b7bdd6
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-08-21 19:48:04 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["El perro", "Mi tia", "El repartidor", "Mi pajaro", "Mi conejo"];
+  let accion = ["se comio", "me rompio", "me perdio"];
+  let que = ["mi tarea", "mi telefono", "el auto"];
+  let cuando = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "cuando me estaba ejercitando",
+    "durante el almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  function getRandomText(array) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  let quienTexto = getRandomText(quien);
+  let accionTexto = getRandomText(accion);
+  let queTexto = getRandomText(que);
+  let cuandoTexto = getRandomText(cuando);
+
+  let oracion =
+    quienTexto + " " + accionTexto + " " + queTexto + " " + cuandoTexto + ".";
+  console.log(oracion);
+
+  document.getElementById("excuse").innerHTML = oracion;
 };
